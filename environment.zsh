abfd69e133a56a6b73edf4e1e7a76bf46f969324
Dmitry Romanov <romanov@jlab.org>
2016-02-22 21:02:05 +0000

    Comments cleanup

--- environment.zsh
+++ environment.zsh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-
-#Next lines just finds the path of the file
-#Works for all versions,including
-#when called via multple depth soft link,
-#when script called by command "source" aka . (dot) operator.
-#when arg $0 is modified from caller.
-#"./script" "/full/path/to/script" "/some/path/../../another/path/script" "./some/folder/script"
-#SCRIPT_PATH is given in full path, no matter how it is called.
-#Just make sure you locate this at start of the script.
+# Get script directory
 SCRIPT_PATH=$0:a:h;
 
 #set our environment
