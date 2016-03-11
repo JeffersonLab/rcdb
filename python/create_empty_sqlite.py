73a5a92b64274d363605a870accbf8b5c7154431
Dmitry Romanov <romanov@jlab.org>
2016-03-11 08:24:59 +0000

    Unit tests SQLite

--- python/create_empty_sqlite.py
+++ python/create_empty_sqlite.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
         print "Such file already exists. Remove it first or give another path"
         exit(1)
 
-    db = rcdb.ConfigurationProvider("sqlite:///"+args.path)
-    rcdb.model.Base.metadata.create_all(db.engine)
+    db = rcdb.RCDBProvider("sqlite:///"+args.path, check_version=False)
+    rcdb.provider.destroy_all_create_schema(db)
     # create run
     db.create_run(0)
     db.create_run(1)
