8156811abdf0db208ff363758d21ac3f0ef67a5d
Dmitry Romanov <romanov@jlab.org>
2016-03-18 15:43:32 -0400

    First DB migration is updated for older database

--- alembic/versions/34ba183eda63_conditions_time_time_value_sql_schema_.py
+++ alembic/versions/34ba183eda63_conditions_time_time_value_sql_schema_.py
@@ -13,20 +13,21 @@ down_revision = None
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
+from sqlalchemy.sql import text
 from rcdb.model import SchemaVersion
 
 
 def upgrade():
     op.create_table('schema_versions',
                     sa.Column('version', sa.Integer(), autoincrement=False, nullable=False),
-                    sa.Column('created', sa.DateTime(), server_default='now()', nullable=True),
+                    sa.Column('created', sa.DateTime(), nullable=True),
                     sa.Column('comment', sa.String(length=255), nullable=True),
                     sa.PrimaryKeyConstraint('version')
                     )
-    op.drop_column(u'condition_types', 'is_many_per_run')
-    op.add_column(u'conditions', sa.Column('time_value', sa.DateTime(), nullable=True))
-    op.drop_column(u'conditions', 'time')
-    op.alter_column(u'files', 'content',
+    op.drop_column('condition_types', 'is_many_per_run')
+    op.add_column('conditions', sa.Column('time_value', sa.DateTime(), nullable=True))
+    op.drop_column('conditions', 'time')
+    op.alter_column('files', 'content',
                     existing_type=mysql.LONGTEXT(),
                     nullable=False)
     op.bulk_insert(SchemaVersion.__table__,
@@ -37,11 +38,11 @@ def upgrade():
 
 
 def downgrade():
-    op.alter_column(u'files', 'content',
+    op.alter_column('files', 'content',
                     existing_type=mysql.LONGTEXT(),
                     nullable=True)
-    op.add_column(u'conditions', sa.Column('time', mysql.DATETIME(), nullable=True))
-    op.drop_column(u'conditions', 'time_value')
-    op.add_column(u'condition_types', sa.Column('is_many_per_run', mysql.TINYINT(display_width=1), nullable=False))
+    op.add_column('conditions', sa.Column('time', mysql.DATETIME(), nullable=True))
+    op.drop_column('conditions', 'time_value')
+    op.add_column('condition_types', sa.Column('is_many_per_run', mysql.TINYINT(display_width=1), nullable=False))
     op.drop_table('schema_versions')
 
