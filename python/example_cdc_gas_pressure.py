4b3e5fc0aa63e0dea9e765af9bf62939734999a5
Dmitry Romanov <romanovda@gmail.com>
2019-03-04 22:27:30 -0500

    Create new run with rcnd

---
+++ python/example_cdc_gas_pressure.py
@@ -0,0 +1,12 @@
+# import RCDB
+from rcdb.provider import RCDBProvider
+
+# connect to DB
+db = RCDBProvider("mysql://rcdb@hallddb.jlab.org/rcdb")
+
+# select values with query
+table = db.select_values(['cdc_gas_pressure'], run_min=41512, run_max=41540)
+
+for row in table:
+    (run_number, cdc_gas_pressure) = tuple(row)
+    print("{}   {}".format(run_number, cdc_gas_pressure))
