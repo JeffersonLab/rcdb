3c54b86016296910fe87a6105c7573ec7f924780
sqrd.max <sqrd.max@gmail.com>
2023-10-06 03:51:52 +0300

    runs doesn't work

--- rcdb_web/test_conditions/veiws.py
+++ rcdb_web/test_conditions/veiws.py
@@ -7,11 +7,18 @@ from collections import defaultdict
 from rcdb.model import Run, Condition, ConditionType
 from sqlalchemy.orm import subqueryload
 
+from rcdb.provider import RCDBProvider
+
 mod = Blueprint('test_conditions', __name__, url_prefix='/test_conditions')
 
 
 @mod.route('/')
 def index():
     conditions = g.tdb.session.query(ConditionType).order_by(ConditionType.name.asc()).all()
-    return render_template("test_conditions/index.html", conditions=conditions)
+
+    db = RCDBProvider("mysql://localhost/rcdb")
+
+    table = db.select_values(['polarization_angle', 'polarization_direction'], run_min=30000, run_max=30050)
+
+    return render_template("test_conditions/index.html", conditions=conditions, table=table)
     pass
