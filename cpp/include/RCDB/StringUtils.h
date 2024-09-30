72fe7d61a72b447b34518861e400e1b1696b57b8
Christopher Dilks <c-dilks@users.noreply.github.com>
2024-09-30 17:33:53 -0400

    fix: `std::ptr_fun` is deprecated in C++11 (and removed in C++17)

--- cpp/include/RCDB/StringUtils.h
+++ cpp/include/RCDB/StringUtils.h
@@ -38,12 +38,12 @@ public:
 
     // trim from start (in place)
     static inline void ltrim(std::string &s) {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](char c) { return !isspace(c); }));
     }
 
     // trim from end (in place)
     static inline void rtrim(std::string &s) {
-        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+        s.erase(std::find_if(s.rbegin(), s.rend(), [](char c) { return !isspace(c); }).base(), s.end());
     }
 
     // trim from both ends (in place)
