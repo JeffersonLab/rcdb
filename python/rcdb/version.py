8bb31d5a7803613da9fa8eacfd06e530d4639afc
Dmitry Romanov <romanovda@gmail.com>
2025-03-25 14:04:14 -0400

    Version 2.2.10 tag

--- python/rcdb/version.py
+++ python/rcdb/version.py
@@ -1,5 +1,5 @@
 # String version. Used both for setup.py and for internal code
-version = '2.2.9'
+version = '2.2.10'
 
 # version as the tuple of ints
 version_tuple = tuple(int(val) for val in version.split('.'))
