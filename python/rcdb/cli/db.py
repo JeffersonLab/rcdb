e4a120c9e5576e1f5ca8aff95e986ebe0f5eb5dd
Dmitry Romanov <romanovda@gmail.com>
2026-07-10 17:52:20 -0400

    Fix db connection leaks

--- python/rcdb/cli/db.py
+++ python/rcdb/cli/db.py
@@ -24,6 +24,7 @@ def db_command(ctx):
             print("ERROR connection string is missing.")
             exit(1)
         provider.connect(connection_str, check_version=False)
+        ctx.call_on_close(provider.disconnect)
         query = select(SchemaVersion).order_by(SchemaVersion.version.desc())
         schema_version, = provider.session.execute(query).first()
         print("Schema version: {} - '{}'".format(schema_version.version, schema_version.comment))
@@ -88,6 +89,7 @@ def _print_sqlite_table_sizes(engine):
 @pass_rcdb_context
 def update(context):
     provider = RCDBProvider(context.connection_str, check_version=False)
+    click.get_current_context().call_on_close(provider.disconnect)
 
     # Check something exists
     if not sqlalchemy.inspect(provider.engine).has_table(SchemaVersion.__tablename__):
@@ -117,6 +119,7 @@ def update(context):
         return
 
     provider = RCDBProvider(context.connection_str, check_version=False)
+    click.get_current_context().call_on_close(provider.disconnect)
 
     # Create alias table
     Alias.__table__.create(provider.engine)
@@ -181,6 +184,7 @@ def init(context, drop_all, no_defaults, confirm, add_tests, add_cpp_tests):
     # That we will need for DB
     metadata = rcdb.model.Base.metadata
     provider = RCDBProvider(context.connection_str, check_version=False)
+    click.get_current_context().call_on_close(provider.disconnect)
 
     # Drop all if needed
     if drop_all:
